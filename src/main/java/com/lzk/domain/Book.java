988d06edefc524b9cff8f54fb3b3c7c90439e6dd
zikangliu <zikangliu0@gmail.com>
2023-07-26 21:13:22 +0800

    sss-case

---
+++ src/main/java/com/lzk/domain/Book.java
@@ -0,0 +1,53 @@
+package com.lzk.domain;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+
+    private Integer id;
+    private String name;
+    private String type;
+    private String description;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
